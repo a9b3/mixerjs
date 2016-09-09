944c8d329487f8fd44f60c06e98a91fea6352783
Sam Lau <esayemm@gmail.com>
2016-09-08 21:22:04 -0700

    update scroll-behavior

--- src/app/index.js
+++ src/app/index.js
@@ -10,9 +10,9 @@ import useScroll from 'scroll-behavior'
 import { AppContainer } from 'react-hot-loader'
 import Root from './root.js'
 
-if (CONFIG.debug === false && navigator.serviceWorker) {
-  navigator.serviceWorker.register('/service-worker.js')
-}
+// if (CONFIG.debug === false && navigator.serviceWorker) {
+//   navigator.serviceWorker.register('/service-worker.js')
+// }
 
 const history = useScroll(browserHistory)
 
