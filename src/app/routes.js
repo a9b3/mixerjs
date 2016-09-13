91c7d05a743b39c3d5ac21879bd2cc9be948ec33
Sam Lau <esayemm@gmail.com>
2016-09-12 23:45:48 -0700

    compat checker

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
+import CompatCheck from './containers/compat-check/compat-check.js'
 import Main from './containers/main/main.js'
 import Demo from './demo/demo.js'
 
 export default (
-  <Route path='/'>
+  <Route path='/' component={CompatCheck}>
     <IndexRoute component={Main} />
     <Route path='demo' component={Demo} />
   </Route>
