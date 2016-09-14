11e21553a6314fa6fa5f7b08a9f986e379f7bb20
Sam Lau <esayemm@gmail.com>
2016-09-14 00:11:46 -0700

    take on decorated component name

--- src/app/helpers/pure-render.js
+++ src/app/helpers/pure-render.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react'
 import shallowCompare from 'react-addons-shallow-compare'
 
 export default (ComposedComponent) => class extends Component {
+  static displayName = ComposedComponent.displayName
+
   shouldComponentUpdate(nextProps, nextState) {
     return shallowCompare(this, nextProps, nextState)
   }
